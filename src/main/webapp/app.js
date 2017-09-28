8a1453c87290165efc3d9982516d18e937cb94d6
Varro Szilard <playszilard@gmail.com>
2017-09-28 19:24:20 +0300

    gallery working with angular and contact tab has google maps api

--- src/main/webapp/app.js
+++ src/main/webapp/app.js
@@ -1,5 +1,15 @@
 (function() {
 	var app = angular.module('hotelTristar', []);
+	var images = ["images/hotel-1.jpg", "images/hotel-2.jpg", "images/hotel-3.jpg"];
+
+	app.controller('GalleryController', function() {
+		this.images = images;
+		this.imageIndex = 0;
+		this.currentImageChange = function(imageNumber) {
+			console.log(imageNumber);
+			this.imageIndex = imageNumber || 0;
+		};
+	});
 
 	app.directive("home", function() {
 		return {
