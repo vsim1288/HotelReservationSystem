3f5df8c05c194e63bc31fb99494635f7c5331193
Varro Szilard <szilard_szili@yahoo.com>
2018-02-12 17:58:39 +0200

    fix fragments, welcome message and role accesses

--- src/main/java/com/hotel/config/SpringSecurityConfig.java
+++ src/main/java/com/hotel/config/SpringSecurityConfig.java
@@ -6,12 +6,14 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.core.GrantedAuthorityDefaults;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
@@ -30,48 +32,45 @@ public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
 	@Value("${spring.queries.roles-query}")
 	private String rolesQuery;
 
-	@Override
-	protected void configure(AuthenticationManagerBuilder auth)
-			throws Exception {
-		final Logger log = LoggerFactory.getLogger(SpringSecurityConfig.class);
-		
-		log.info(usersQuery + " " + rolesQuery);
-		auth.
-			jdbcAuthentication()
-				.usersByUsernameQuery(usersQuery)
-				.authoritiesByUsernameQuery(rolesQuery)
-				.dataSource(dataSource)
-				.passwordEncoder(bCryptPasswordEncoder);
+	@Bean
+	GrantedAuthorityDefaults grantedAuthorityDefaults() {
+		return new GrantedAuthorityDefaults(""); // Remove the ROLE_ prefix
 	}
-	
+
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		final Logger log = LoggerFactory.getLogger(SpringSecurityConfig.class);
+
+		log.info(usersQuery + " " + rolesQuery);
+		auth.jdbcAuthentication().usersByUsernameQuery(usersQuery).authoritiesByUsernameQuery(rolesQuery)
+				.dataSource(dataSource).passwordEncoder(bCryptPasswordEncoder);
+	}
+
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
-		
+
 		http.csrf().disable()
 			.authorizeRequests()
 				.antMatchers("/", "/welcome", "/contact", "/gallery", "/register").permitAll()
 				.antMatchers("/admin/**").hasAuthority("ADMIN")
-				.anyRequest().authenticated()
+				.antMatchers("/booking").hasAuthority("USER")
 				.and()
 				.formLogin()
-				.loginPage("/login")
-				.permitAll()
-				.defaultSuccessUrl("/booking")
-				.usernameParameter("username")
-				.passwordParameter("password")
+					.loginPage("/login").permitAll()
+					.defaultSuccessUrl("/booking")
+					.usernameParameter("username")
+					.passwordParameter("password")
 				.and()
 				.logout()
-				.logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
-				.logoutSuccessUrl("/")
+					.logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
+					.logoutSuccessUrl("/")
 				.and()
 				.exceptionHandling()
-				.accessDeniedPage("/access-denied");
+					.accessDeniedPage("/access-denied");
 	}
-	
+
 	@Override
 	public void configure(WebSecurity web) throws Exception {
-	    web
-	       .ignoring()
-	       .antMatchers("/resources/**", "/static/**", "/stylesheets/**", "/js/**", "/images/**");
+		web.ignoring().antMatchers("/resources/**", "/static/**", "/stylesheets/**", "/js/**", "/images/**");
 	}
 }
